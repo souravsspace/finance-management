d04869af35c0cbee851ae85d1542e8674644872c
Sourav <souravsspace@gmail.com>
2024-08-17 22:38:30 +0600

    fixed hono

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -7,13 +7,6 @@ export const runtime = "edge"
 
 const app = new Hono().basePath("/api")
 
-app.onError((err, c) => {
-   if (err instanceof HTTPException) {
-      return err.getResponse()
-   }
-   return c.json({ message: "Internal Server Error" }, 500)
-})
-
 const routes = app.route("/accounts", accounts)
 
 export const GET = handle(app)
